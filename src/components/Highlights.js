70529889633d65b0f16964250a8902f068a081cc
MwirigiBrianBob21 <brian.mwirigi@students.moringaschool.com>
2022-10-07 04:09:34 +0300

    Display highlights

---
+++ src/components/Highlights.js
@@ -0,0 +1,30 @@
+const Highlights = () => {
+    const[data, setData] = useState([])
+    useEffect(()=>{
+        axios({
+            method: "GET",
+            url: `https://www.scorebat.com/video-api/v3/feed/?token=Mjg3NThfMTY2NTEwMDAxNF82YzY5MmU3N2I4ZmQzNGE1Yjc3MWZmZDQwN2JjZWQ2ZTA0ZWIxZmY1`
+        }).then((res)=> {
+            // console.log(res.data);
+            setData(res.data.response)
+        })
+        .catch((err) => console.log(err))
+    }, []);
+    return(
+        <div className="live-content">
+            {data.map((item)=>(
+                <div className="itemDiv" key={item.title} onClick={()=> window.open(item.matchviewUrl)}>
+                    <div className="itemHeading">
+                        <h4>{item.title}</h4>
+
+                    
+                    
+                    </div>
+                   <div className="itemImage">
+                    <img src={item.thumbnail} alt="#"/>
+                   </div>
+                   
+                    </div>
+                
+            ))}
+        </div>
